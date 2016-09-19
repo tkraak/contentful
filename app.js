41386da3438fbfb2f5daa8627bf6d458f3dcdd84
Tom Kraak <tom@backroom.io>
2016-09-19 15:19:03 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HardSourcePlugin = require('hard-source-webpack-plugin')
+const Contentful = require('spike-contentful')
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('babel-preset-latest')
+const pageId = require('spike-page-id')
+const locals = {}
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: {
+    html: '*(**/)*.sgr',
+    css: '*(**/)*.sss'
+  },
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', '_cache/**', 'readme.md'],
+  plugins: [
+    new Contentful({
+      addDataTo: locals,
+      accessToken: '3ca35275e1d8a3a642c38143b59341bc727defc76e5e7893fd6f948b569ecabe',
+      spaceId: '5b0pqhu33f9d',
+      contentTypes: [
+        {
+          name: 'posts',
+          id: '2wKn6yEnZewu2SCCkus4as'
+        }
+      ],
+      json: 'data/posts.json'
+    })
+  ],
+  reshape: (ctx) => {
+    return htmlStandards({
+      webpack: ctx,
+      locals: { pageId: pageId(ctx), foo: 'bar', locals }
+    })
+  },
+  postcss: (ctx) => {
+    return cssStandards({ webpack: ctx })
+  },
+  babel: { presets: [jsStandards] },
+  plugins: [
+    new HardSourcePlugin({
+      environmentPaths: { root: __dirname },
+      recordsPath: path.join(__dirname, '_cache/records.json'),
+      cacheDirectory: path.join(__dirname, '_cache/hard_source_cache')
+    })
+  ]
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Experimenting with spike-contentful
+
+The `posts` content type on contentful:
+
+[https://cdn.contentful.com/spaces/5b0pqhu33f9d/entries?access_token=3ca35275e1d8a3a642c38143b59341bc727defc76e5e7893fd6f948b569ecabe&content_type=2wKn6yEnZewu2SCCkus4as&order=sys.createdAt](https://cdn.contentful.com/spaces/5b0pqhu33f9d/entries?access_token=3ca35275e1d8a3a642c38143b59341bc727defc76e5e7893fd6f948b569ecabe&content_type=2wKn6yEnZewu2SCCkus4as&order=sys.createdAt)
+
+
