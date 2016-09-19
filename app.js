cb6cad3d6350240b53a56042ef255747a85e5078
Tom Kraak <tom@backroom.io>
2016-09-19 15:33:29 -0400

    fix plugin config

--- app.js
+++ app.js
@@ -14,20 +14,6 @@ module.exports = {
     css: '*(**/)*.sss'
   },
   ignore: ['**/layout.sgr', '**/_*', '**/.*', '_cache/**', 'readme.md'],
-  plugins: [
-    new Contentful({
-      addDataTo: locals,
-      accessToken: '3ca35275e1d8a3a642c38143b59341bc727defc76e5e7893fd6f948b569ecabe',
-      spaceId: '5b0pqhu33f9d',
-      contentTypes: [
-        {
-          name: 'posts',
-          id: '2wKn6yEnZewu2SCCkus4as'
-        }
-      ],
-      json: 'data/posts.json'
-    })
-  ],
   reshape: (ctx) => {
     return htmlStandards({
       webpack: ctx,
@@ -43,6 +29,18 @@ module.exports = {
       environmentPaths: { root: __dirname },
       recordsPath: path.join(__dirname, '_cache/records.json'),
       cacheDirectory: path.join(__dirname, '_cache/hard_source_cache')
+    }),
+    new Contentful({
+      addDataTo: locals,
+      accessToken: '3ca35275e1d8a3a642c38143b59341bc727defc76e5e7893fd6f948b569ecabe',
+      spaceId: '5b0pqhu33f9d',
+      contentTypes: [
+        {
+          name: 'posts',
+          id: '2wKn6yEnZewu2SCCkus4as'
+        }
+      ],
+      json: 'data/posts.json'
     })
   ]
 }
